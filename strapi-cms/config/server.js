9e8c24783ac39360d2ca14e9312b5c95933ce622
Marcos <marcob@avetti.ca>
2021-06-07 14:06:29 -0400

    strapi commit
    first commit strapi + gatsby

---
+++ strapi-cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e44ef4f5a886953513d2dd3952140bb6'),
+    },
+  },
+});
---
+++ strapi-cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '88554079-ee05-40dc-bc28-a8d8e8dd095a'
+};
